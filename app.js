37d6508f738a907395454501c5cc9040b6cf02cd
sdepold <sascha@depold.com>
2011-09-07 14:38:16 +0200

    removed manual logging logic due to node-imageable's reporting support

--- app.js
+++ app.js
@@ -8,40 +8,23 @@ var express   = require('express')
 // Configuration
 app.configure(function(){
   var start = Date.now()
-  
+
   app.use(express.logger({ format: ':date - :method :url' }))
   app.use(express.bodyParser())
   app.use(express.methodOverride())
   app.use(imageable(config, {
-    before: function(stats) {},
-    after: function(stats) {
-      if(config.statsd && ((Date.now() - (config.statsd.interval * 1000)) > start)) {
-        var data = stats.format()
-
-        for(var key in data) {
-          var url = config.statsd.urls[key].replace("%{by}", data[key])
-            , cmd = "curl --insecure '" + url + "'"
-          
-          console.log("EXECUTING:", cmd)
-          require('child_process').exec(cmd, function(err, stdout, stderr) {
-            console.log("REQUEST-RESULT: ", err, stdout)
-          })
-        }
-
-        stats.reset()
-        start = Date.now()
-      }
-    }
+    before: function() {},
+    after: function(stats) {}
   }))
   app.use(app.router)
 })
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })) 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
 })
 
 app.configure('production', function(){
-  app.use(express.errorHandler()) 
+  app.use(express.errorHandler())
 })
 
 // Routes
