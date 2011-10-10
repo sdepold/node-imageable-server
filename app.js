324dad7fb06870ff9f1e2fc5f46894f75f9ea323
sdepold <sascha@depold.com>
2011-10-10 09:33:36 +0200

    require 0.8.5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express   = require('express')
   , imageable = require("imageable")
+  , imageable = require("/Users/sdepold/Projects/node-imageable/index")
   , connect   = require('connect')
   , http      = require("http")
   , fs        = require("fs")
