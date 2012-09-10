29ee541e0fd11ffdf1ab49f4798f131d1a1d2482
sdepold <sascha@depold.com>
2012-09-10 12:39:33 +0200

    busterjs-ify

---
+++ spec/app.spec.js
@@ -0,0 +1,23 @@
+var buster         = require('buster')
+  , ExpressTestBot = require('express-test-bot')
+
+buster.spec.expose()
+
+describe('ImageableServer', function() {
+  before(function() {
+    this.server = new ExpressTestBot()
+  })
+
+  describe('GET /', function() {
+    it('returns not so friendly home page', function(done) {
+      this.server.get('/', function(err, stdout, stderr) {
+        expect(stdout).toMatch('This is not the page you are looking for.')
+        done()
+      })
+    })
+
+    it("fails", function() {
+      expect(1).toEqual(2)
+    })
+  })
+})
